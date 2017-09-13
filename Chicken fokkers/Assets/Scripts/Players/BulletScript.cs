d81a1466ad572880a68a6386e467b30a9c274221
David Ball <david@ongoingworlds.com>
2017-09-13 21:30:47 +0100

    Bulletscript

---
+++ Chicken fokkers/Assets/Scripts/Players/BulletScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+	public GameObject Owner;
+	private float speed = 500f;
+	public Rigidbody2D rb;
+	public GameObject Explosion;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("create new bullet");
+
+		rb.AddForce(transform.right * speed);
+
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+
+		Debug.Log("bullet hits = "+other.name);
+
+        if (other.tag == "Player"){
+
+        	if(other.GetComponent<DamageController>()){
+        		other.GetComponent<DamageController>().HitByBullet();
+
+        		Instantiate(Explosion, transform.position, transform.rotation);
+
+            	Destroy(gameObject);
+    		} 
+
+    		
+        } else if (other.tag == "Ground"){
+			Instantiate(Explosion, transform.position, transform.rotation);
+
+        	Destroy(gameObject);
+        } 
+    }
+}
---
+++ Chicken fokkers/Assets/Scripts/Players/ShootController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour {
+
+	public Transform shootPos; 
+	public Transform ShootRayFar;
+	public bool shooting = false;
+	private float fireRate = 10f;
+	public GameObject Bullet; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Update (){
+		RaycastHit2D shootingHit = Physics2D.Linecast(transform.position, ShootRayFar.position, 1 << LayerMask.NameToLayer("Player"));
+		Debug.DrawLine(transform.position, ShootRayFar.position, Color.red);
+
+		if(shootingHit){
+			Debug.Log("hit");
+			shooting = true;
+			InvokeRepeating("FireBullet", 0, fireRate);
+		} else {
+			shooting = false;
+			CancelInvoke("FireBullet");
+		}
+	}
+
+	void FireBullet() {
+		GameObject newBullet = Instantiate(Bullet, transform.position, transform.rotation);
+		
+		//--set the owner of this bullet
+		newBullet.GetComponent<BulletScript>().Owner = gameObject;
+	}
+
+
+	
+}

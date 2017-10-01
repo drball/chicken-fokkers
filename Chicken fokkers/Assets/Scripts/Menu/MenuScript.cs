168a22aee4bc2f4d53a6a0c1b38275328464f625
David Ball <david@ongoingworlds.com>
2017-10-01 17:30:51 +0100

    Menu screen animations

---
+++ Chicken fokkers/Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+	public GameObject LoadingPanel;
+
+	// Use this for initialization
+	void Start () {
+		LoadingPanel.SetActive(false);
+	}
+	
+	void StartGame(){
+		//--show loading panel because there's a delay
+		LoadingPanel.SetActive(true);
+		
+		Application.LoadLevel("main");
+	}
+
+	void RateBtnPressed(){
+
+	}
+
+	void TwitterBtnPressed(){
+		Application.OpenURL("https://www.twitter.com/davidonionball");
+	}
+
+	void LikeBtnPressed(){
+		Application.OpenURL("https://www.facebook.com/BotSumoGame/");
+	}
+
+	void FacebookBtnPressed(){
+
+	}
+}

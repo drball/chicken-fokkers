43fccf2bc7a40865c49c5c9b806a206110feeee0
David Ball <david@ongoingworlds.com>
2017-10-25 00:23:44 +0100

    Added engine sfx from standard assets

---
+++ Chicken fokkers/Assets/Scripts/Players/EngineSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineSound : MonoBehaviour {
+
+	public AudioSource EngineSfx;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

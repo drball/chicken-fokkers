5c8cda7c6f1732a58542b5e19d98e2918a685365
David Ball <david@ongoingworlds.com>
2018-10-08 07:08:45 +0100

    New tileset from Figma

---
+++ Chicken fokkers/Assets/Scripts/GameControllerStory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerStory : MonoBehaviour {
+
+	public PlayerController Player1Controller;
+	public bool roundActive = false;
+	// public GameObject ScoreModal;
+	public GameObject PlayAgainBtn;
+	public GameObject Player1ScoreText;
+	public LevelsController LevelsController;
+	public AdvertsController AdvertsController;
+	public GameObject Countdown;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

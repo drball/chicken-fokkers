a0d5ee82a40f8c90d7dc0c6faeee13ba9b0c9fc6
David Ball <david@ongoingworlds.com>
2018-10-17 08:00:26 +0100

    Added some scripts to chicen ragdoll

---
+++ Chicken fokkers/Assets/AddImpulseForce.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//--used for adding a force to instantiated ragdoll when hit
+
+public class AddImpulseForce : MonoBehaviour {
+
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void AddForce(Vector2 forceAmt){
+		rb.AddForce(forceAmt, ForceMode2D.Impulse);
+	}
+}
---
+++ Chicken fokkers/Assets/BlinkThenRemove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkThenRemove : MonoBehaviour {
+
+	public float delay = 2.5f;
+	public Renderer[] childRends;
+
+	// Use this for initialization
+	void Start () {
+		childRends = GetComponentsInChildren<Renderer>( ) as Renderer[];
+		Invoke("StartFlashing", delay);
+	}
+	
+    void StartFlashing(){
+    	Debug.Log("start flashing");
+    	StartCoroutine("Blink");
+    }
+
+    IEnumerator Blink(){
+    	Hide();
+    	yield return new WaitForSeconds(0.04f);
+    	Show();
+    	yield return new WaitForSeconds(0.04f);
+    	Hide();
+    	yield return new WaitForSeconds(0.04f);
+    	Show();
+    	yield return new WaitForSeconds(0.04f);
+    	Hide();
+    	Destroy(gameObject);
+    }
+
+    void Hide(){
+		//-disable renderer of all children
+    	foreach( Renderer childRend in childRends ){
+            childRend.enabled = false;
+            // Debug.Log("make "+childRend+" hidden");
+        }
+
+    }
+
+    void Show(){
+		//-enable renderer of all children
+    	foreach( Renderer childRend in childRends ){
+            childRend.enabled = true;
+            // Debug.Log("make "+childRend+" show");
+        }
+    }
+}

ba8072c1e25caf5ae498d4d02f9c0b67f9c08d53
David Ball <david@ongoingworlds.com>
2018-10-20 14:14:53 +0100

    Weird rotational exceptions

--- Chicken fokkers/Assets/Scripts/Enemies/LookAtPlayer.cs
+++ Chicken fokkers/Assets/Scripts/Enemies/LookAtPlayer.cs
@@ -2,22 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurretShooting : MonoBehaviour {
+public class LookAtPlayer : MonoBehaviour {
 
 	public GameObject target;
-	private Vector3 initialScale;
 	public GameObject Rotator;
 	public bool isActive = false;
 	public float turnSpeed = 0.05f;
-	public float fireRate = 1f;
-	public GameObject Bullet;
-	public Transform FireFrom;
 	public float minAngle;
 	public float maxAngle;
 	private float angle;
-	public float initialAngle;
-	
-	// Update is called once per frame
+	private float initialAngle;
+
+	void Start(){
+		initialAngle = 210f;
+	}
+
 	void Update () {
 
 		if(isActive){
@@ -25,58 +24,43 @@ public class TurretShooting : MonoBehaviour {
 			//--get angle towards target
 			angle = Mathf.Atan2(target.transform.position.y - Rotator.transform.position.y, target.transform.position.x - Rotator.transform.position.x) * Mathf.Rad2Deg;
 
-			// Debug.Log("angle = "+angle);
+			// Debug.Log("head angle = "+angle);
 
 			//--rotate towards target
 			if((angle >= minAngle) && (angle <= maxAngle)){
 				Rotator.transform.rotation = Quaternion.Lerp(Rotator.transform.rotation, Quaternion.Euler(0f, 0f, angle), Time.time * turnSpeed);
 			}
 
-
+			if(!target.activeSelf){
+				isActive = false;
+				target = null;
+			} 
+	 	
 	 	} else {
-	 		CancelInvoke("Shoot");
 	 		target = null;
 	 		//--go back to start rotation
 	 		Rotator.transform.rotation = Quaternion.Lerp(Rotator.transform.rotation, Quaternion.Euler(0f, 0f, initialAngle), Time.time * turnSpeed);
 	 	}
+
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		Debug.Log("turret detected "+other.name);
+		// Debug.Log("head detected "+other.name);
 
 		if(isActive == false){
 			Debug.Log("turret ready to shoot");
 			if (other.transform.name == "PlayerCollider"){
-				
 				isActive = true;
 				target = other.transform.parent.gameObject;
-				Debug.Log("tbc turret has targetted "+target.name);
-				
-				InvokeRepeating("Shoot", fireRate, 1);
 			}
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D other) {
+
 		if(isActive == true && other.transform.parent.name == target.name){
-			Debug.Log("deactivate turret "+transform.name);
+			Debug.Log("deactivate head "+transform.name);
 			isActive = false;
 		}
 	}
-
-	void Shoot(){
-
-		Debug.Log("attempt shoot");
-
-		if(target.activeSelf){
-			GameObject newBullet = Instantiate(Bullet, FireFrom.position, FireFrom.rotation);
-			Debug.Log("New bullet from "+transform.name+", fired at angle = "+angle);
-
-		} else {
-			isActive = false;
-			target = null;
-		}
-		
-	}
-
 }

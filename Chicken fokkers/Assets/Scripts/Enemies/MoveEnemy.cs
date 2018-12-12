5b741bf3b0da322fb575dbe208b1a89809a9bdb9
David Ball <david@ongoingworlds.com>
2018-12-12 07:47:31 +0000

    Added plane enemy

---
+++ Chicken fokkers/Assets/Scripts/Enemies/MoveEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float speed = 10f;
+	public BoxCollider activateCollider;
+
+	// Use this for initialization
+	void Start () {
+		rb.AddForce(transform.right * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

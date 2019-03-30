2daff4f203bbeb836877cec4457bd8d40e5ac34d
David Ball <david@ongoingworlds.com>
2019-03-30 22:23:13 +0000

    Added iOS stuff

---
+++ Chicken fokkers/Assets/HideOnIOS.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideOnIOS : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		#if UNITY_IOS
+			gameObject.SetActive(false);
+		#endif
+	}
+	
+}
